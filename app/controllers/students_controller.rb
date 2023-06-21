e150836659bfdb1b356deaead8a04313077ec97f
William Mtemi Mponzi <willbrian009@gmail.com>
2023-06-21 13:30:05 +0300

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # students = Student.all
+    # render json: students
+
+    if params[:name].present?
+      students = Student.where("first_name LIKE :query OR last_name LIKE :query", query: "%#{params[:name]}%")
+    else
+      students = Student.all
+    end
+
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
